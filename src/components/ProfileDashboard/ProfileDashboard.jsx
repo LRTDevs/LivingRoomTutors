229b2b0bde91ee54963af8f8b8b0d89215305c6e
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-06-23 11:34:15 -0500

    added guidline text

--- src/components/ProfileDashboard/ProfileDashboard.jsx
+++ src/components/ProfileDashboard/ProfileDashboard.jsx
@@ -1,7 +1,21 @@
 import Header from '../Header/Header';
 
 function ProfileDashboard() {
-  return <Header />;
+  return (
+  
+  
+
+  <div>
+  <Header />
+<h1>Welcome To The Dashboard! </h1>
+
+<h3> At a glance</h3>
+
+
+
+  </div>
+  
+  ) ;
 }
 
 export default ProfileDashboard;
