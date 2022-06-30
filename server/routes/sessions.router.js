a988aa640791ba34611fdfdf90d3e2fe23a492ea
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-06-30 09:31:57 -0500

    schedule dates post to db for tutor

--- server/routes/sessions.router.js
+++ server/routes/sessions.router.js
@@ -3,27 +3,50 @@ const pool = require("../modules/pool");
 const router = express.Router();
 
 
-router.post('/', (req, res) => {
-    console.log('Tutor Session req.body--------->', req.body)
-    const sqlText = `
-  INSERT INTO "schedule" ("primary_date", "secondary_date", "tertiary_date", "user_id")
-  VALUES ($1, $2, $3, $4);
-  `;
-    const sqlValues = [
+// POST route is for tutors recommending times
+// PUT route is for tutees accepting/rejecting times
+
+
+// will handle a tutor suggesting times:
+router.post('/', async (req, res) => {
+  
+  // BALLOON! We re-did this route big time:
+  try {
+    // find out the current tutor's tutor_id, using the req.body.user.id
+    const getTutorIdQuery = `
+      SELECT "id" FROM "tutors"
+        WHERE tutors.user_id=$1
+    `
+    const tutorIdRes = await pool.query(getTutorIdQuery, [req.user.id]);
+    const tutorId = tutorIdRes.rows[0].id
+
+    // insert three rows into the sessions table, each row looks something like:
+    const insertScheduleQuery = `
+      INSERT INTO "schedule"
+        ("tutor_id", "tutee_id", "date")
+        VALUES
+        ($1, $2, $3),
+        ($1, $2, $4),
+        ($1, $2, $5)
+    `
+    const insertScheduleSqlValues = [
+      tutorId,
+      req.body.tutee_id,
       req.body.primaryDate,
       req.body.secondaryDate,
-      req.body.tertiaryDate,
-      req.user.id
+      req.body.tertiaryDate
     ]
-    pool
-      .query(sqlText, sqlValues)
-      .then((result) => {
-        res.sendStatus(200);
-      })
-      .catch((error) => {
-        console.log('Error in TUTOR SESSIONS POST request:', error);
-      });
-  });
+
+    await pool.query(insertScheduleQuery, insertScheduleSqlValues);
+
+    res.sendStatus(201);
+  } catch (dbErr) {
+    console.error(dbErr);
+    res.sendStatus(500);
+  }
+});
+
+
 
 
 
