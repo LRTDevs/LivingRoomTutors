e18a20713d4c49ec541d937d7b9021ecf73e0308
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-06-30 20:28:50 -0500

    mapped matches for tutee dropdown

--- src/components/Sessions/Sessions.jsx
+++ src/components/Sessions/Sessions.jsx
@@ -5,6 +5,7 @@ import { Accordion, Button, Form } from "react-bootstrap";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import {useHistory} from "react-router-dom"
 
 
 function Sessions() {
@@ -13,19 +14,22 @@ function Sessions() {
     if (user.isTutor === false) {
       dispatch({
         type: "FETCH_TUTOR_SESSIONS",
+        type:"FETCH_MATCHED_TUTEES"
       });
     }
   }, []);
 
   const dispatch = useDispatch();
-
+  const history = useHistory();
   const user = useSelector((store) => store.user);
+  const matches = useSelector((store) => store.matches);
+
 
   const [primaryDate, setPrimaryDate] = useState([]);
   const [secondaryDate, setSecondaryDate] = useState([]);
   const [tertiaryDate, setTertiaryDate] = useState([]);
 
-  const AddSessions = () => {
+  const AddSessions = (tutee_id) => {
     const newSessions = {
       primaryDate: primaryDate,
       secondaryDate: secondaryDate,
@@ -33,16 +37,18 @@ function Sessions() {
     };
 
     console.log("newSessions------------>", newSessions);
+    console.log("Matches------------>", matches);
 
     dispatch({
       type: "ADD_TUTOR_SESSIONS",
-      payload: { ...newSessions, tutee_id: 1 },
+      payload: { ...newSessions, tutee_id: tutee_id },
     });
       //hardcoded for now
       // BALLOON! added a hardcoded tutee_id value to test this out
       // remember that it was super important to have dummy data that made
       // sense in order to test all this out.
       //dropdown that maps tutees.id's of tutor. 
+      // history.push('/ProfileDashboard')
   };
 
   return (
@@ -63,6 +69,52 @@ function Sessions() {
                       Please select three dates and times for possible sessions.
                       Sessions will be send to tutee for confirmation.
                     </span>
+
+
+                    <div className="formQandA">
+            <label className="customLabel" htmlFor="gradeLevel">
+              Select Student Name 
+              <span className="requiredField"> *</span>
+            </label>
+
+            <Form.Select
+              id="gradeLevel"
+              className="selectGradeDropdown"
+              aria-label="gradeLevel"
+              // value={matches}
+         
+            >
+               {matches.map((match) => {
+                console.log('match reducer in map',match)
+                return (
+                  
+                  <option value = {match.id} onClick={() => AddSessions(match.tutee_id)} key={match.id}>
+                    {match.tutee_firstname} -
+                  </option>
+                );
+              })}
+
+/// .filter where user.id === match.tutor.id
+
+
+
+              <option value="">Select </option>
+            
+              {/* <option value="1st">1st Grade</option>
+              <option value="2nd">2nd Grade</option> */}
+  
+            </Form.Select>
+          </div>
+
+
+
+
+
+
+
+
+
+
 
                     <Form.Group className="mb-3">
                       <Form.Label
