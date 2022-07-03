94fd8d479d5e87360da29da392be7f665d9c0041
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-03 15:09:42 -0500

    added name and isRejected or confirmed to selectedSessions on ProfileDashboard

--- src/components/ProfileDashboard/ProfileDashboard.jsx
+++ src/components/ProfileDashboard/ProfileDashboard.jsx
@@ -4,43 +4,34 @@ import Logo from "../../images/logoCrop.png";
 
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import { DateTime } from "luxon";
 
 import Nav from "../Nav/Nav";
 import { Card, Col, Container } from "react-bootstrap";
-import UserPage from '../UserPage/UserPage';
-import AdminNavBar from '../AdminNavBar/AdminNavBar';
+import UserPage from "../UserPage/UserPage";
+import AdminNavBar from "../AdminNavBar/AdminNavBar";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import React from 'react';
-
-
+import React from "react";
 
 function ProfileDashboard() {
-
   useEffect(() => {
     dispatch({
-        type: 'FETCH_PROFILE_PIC',
-        payload: user.id
-    })
+      type: "FETCH_PROFILE_PIC",
+      payload: user.id,
+    });
     dispatch({
       type: "FETCH_SELECTED_SESSIONS",
     });
+  }, []);
 
-}, [])
+  const selectedSessions = useSelector((store) => store.selectedSessions);
 
-const selectedSessions = useSelector((store) => store.selectedSessions);
-
-
-  const user = useSelector((store)=> store.user)
+  const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
-
-  
-
-
 
   return (
     <div>
-
       <Col lg={{ span: 8, offset: 3 }}>
         <Header />
         {user.isAdmin === true ? <AdminNavBar /> : <Nav />}
@@ -52,15 +43,24 @@ const selectedSessions = useSelector((store) => store.selectedSessions);
 
               <h1>Welcome To The Dashboard, {user.username}!</h1>
 
-{/* should show only isConfirm === true */}
-              {selectedSessions
-                      .map((session) => {
-                        console.log("sessions in dashboard", session);
-                        return (
-                          < h3 key={session.id}> {session.date} {session.student_first_name}</h3>
-                        );
-                      })}
-
+              {/* should show only isConfirm === true */}
+              {selectedSessions.map((session) => {
+                console.log("sessions in dashboard", session);
+                return (
+                  <h1 key={session.id}>
+                    {" "}
+                    {DateTime.fromISO(session.date).toLocaleString(
+                      DateTime.DATETIME_MED
+                    )}
+                   <> -{session.student_first_name}
+                   {session.isBooked == true ? <p>Confirmed ✓</p> : <p>Rejected </p>}
+                   
+                   
+                   
+                   </>
+                  </h1>
+                );
+              })}
             </Card.Body>
           </Card>
         </Container>
