448e62d743c898a2df6cd01e2ff2a93f7b16640d
Jake McInerney <jakemcinerney1@gmail.com>
2022-07-04 17:02:28 -0500

    added DateTime and luxon to ProfileDashboard component

--- src/components/ProfileDashboard/ProfileDashboard.jsx
+++ src/components/ProfileDashboard/ProfileDashboard.jsx
@@ -9,6 +9,7 @@ import AdminNavBar from '../AdminNavBar/AdminNavBar';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import React from 'react';
+import { DateTime } from 'luxon';
 
 function ProfileDashboard() {
   useEffect(() => {
